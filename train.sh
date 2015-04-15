f7a4ef2856ee5fe37cfb328d65b2e86ce468a950
GitLab <gitlab@localhost>
2015-04-15 10:49:13 +0800

    run test

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-java -Xmx6000m -cp jmlt.jar org.jmlp.classify.svm_struct.source.svm_struct_main -c 5000 example/train.txt example/model
+java -Xmx6000m -cp out/jmlt.jar org.jmlp.classify.svm_struct.source.svm_struct_main -c 5000 example/test.txt example/model
 
 #java -Xmx2000m -cp jmlt.jar org.jmlp.classify.svm_struct.source.svm_struct_classify example/test.txt example/model example/prediction
