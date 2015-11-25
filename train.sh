be24fa872429a13ef3d87402f62c0aaa3c432389
GitLab <gitlab@localhost>
2015-11-26 00:43:42 +0800

    train

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-#java -Xmx1000m -cp jmlt.jar org.click.classify.svmstruct.model.MainStruct -c 5000 example/train.txt example/model
+java -Xmx1000m -cp jmlt.jar org.click.classify.svmstruct.model.MainStruct -c 5000 example/train.txt example/model
 
 java -Xmx1000m -cp jmlt.jar org.click.classify.svmstruct.model.ClassifyStruct example/test.txt example/model example/prediction
