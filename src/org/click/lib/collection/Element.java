d9040c3d455676cbb05065db458a8aae7532bebd
liqi6 <liqi6@letv.com>
2015-12-01 16:51:39 +0800

    test

---
+++ src/org/click/lib/collection/Element.java
@@ -0,0 +1,14 @@
+package org.click.lib.collection;
+
+public class Element implements Comparator<Integer>{
+
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+}
